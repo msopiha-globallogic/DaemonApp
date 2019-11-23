a99c66a48cbc17dc8a8689900a3851068804a1df
Mykgailo Sopiha <mykhailo.sopiha@globallogic.com>
2019-11-23 18:22:37 +0200

    Add state processing for testing
    Signed-off-by: Mykgailo Sopiha <mykhailo.sopiha@globallogic.com>

--- main.cpp
+++ main.cpp
@@ -771,9 +771,13 @@ out:
 
 int main() {
     Connection con(8080);
-    if (con.StartListening())
+    if (con.StartListening()) {
         std::cout << "Failed to start listening. Err = " << con.GetLastErrorString()
                   << std::endl;
+        return -1;
+    }
     Session s(con.GetNextConnection(), "cert", "key");
     Token t = s.getSessionToken();
+    std::cout << "Got token state " << t.getState();
+    return 0;
 }
